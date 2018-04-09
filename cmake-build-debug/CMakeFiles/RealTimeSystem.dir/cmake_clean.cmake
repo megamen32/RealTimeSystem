454c00ab9509fe3c418c0ec3f1492e3ec848fbd1
Careviolan <megamen932@gmail.com>
2018-04-09 19:25:21 +0300

    nothing
    Second Coomit

---
+++ cmake-build-debug/CMakeFiles/RealTimeSystem.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RealTimeSystem.dir/main.cpp.o"
+  "RealTimeSystem.pdb"
+  "RealTimeSystem"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RealTimeSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/RealTimeSystem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for RealTimeSystem.
+# This may be replaced when dependencies are built.
